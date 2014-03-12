1f53fdccf23148365f8747827d9baf2bfc316132
Nico Waldispuehl <nico.waldispuehl@gmx.ch>
2014-03-12 20:22:23 +0100

    Some more exercise.

---
+++ hands_on_examples/src/main/java/exercise_05/SomeUsefulSnippets.java
@@ -0,0 +1,14 @@
+package exercise_05;
+
+public class SomeUsefulSnippets {
+  
+  public static void main(String[] args) {
+
+    // Load a file with a path 'some/path' and process it line by line:
+    
+    
+    
+    
+  }
+  
+}
