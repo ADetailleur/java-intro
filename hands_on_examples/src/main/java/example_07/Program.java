4cde419094962e59fc781ca84ff9198180ac62b9
Nico Waldispühl <nw@retorte.ch>
2017-04-23 17:07:55 +0200

    Made weather info location easier changable.

--- hands_on_examples/src/main/java/example_07/Program.java
+++ hands_on_examples/src/main/java/example_07/Program.java
@@ -3,11 +3,13 @@ package example_07;
 public class Program {
 	
 	public static void main(String[] args) {
+		
+		String location = "Zurich City, CH";
 	
 		WeatherInformation weatherInformation = new OpenWeatherMapWeatherInformation();
-		double temperature = weatherInformation.getTemperatureFor("Zurich City, CH");
+		double temperature = weatherInformation.getTemperatureFor(location);
 		
-		System.out.println("Temperature in Zurich: " + temperature);
+		System.out.println("Temperature in '" + location + "': " + temperature);
 	}
 	
 }
