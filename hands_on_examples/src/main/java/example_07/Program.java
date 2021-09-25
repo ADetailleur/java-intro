63eae41810ff253cd0a0057fc46e8c9f9fd80f58
Nico Waldispühl <nw@retorte.ch>
2021-09-25 21:20:46 +0200

    Updated years, tiny improvements

--- hands_on_examples/src/main/java/example_07/Program.java
+++ hands_on_examples/src/main/java/example_07/Program.java
@@ -4,12 +4,18 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
-		String location = "Zurich, CH";
+		String location;
+		if (args.length == 1) {
+			location = args[0];
+		}
+		else {
+			location = "Zurich, CH";
+		}
 	
 		WeatherInformation weatherInformation = new OpenWeatherMapWeatherInformation();
 		double temperature = weatherInformation.getTemperatureFor(location);
 		
-		System.out.println("Temperature in '" + location + "': " + temperature);
+		System.out.println("Temperature in '" + location + "': " + temperature + " °C");
 	}
 	
 }
